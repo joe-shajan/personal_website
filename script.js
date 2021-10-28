0de8ec772e841d68d17be2de9b30388c5f036b8c
joeshajan <joeshajan1551@gmail.com>
2021-10-28 21:12:54 +0530

    first

---
+++ script.js
@@ -0,0 +1,93 @@
+
+$(document).ready(function () {
+    $('#namecheck').hide();
+    $('#namecheck1').hide();
+    $('#subjectcheck').hide();
+    $('#messagecheck').hide();
+    $('#emailcheck').hide();
+    $('#emailcheck1').hide();
+
+  });
+  function validateName() {
+    let nameValue = $('#name').val();
+    var re = /^[A-Za-z]+$/;
+    if (re.test(nameValue) == true) {
+      return true;
+    } else if (nameValue.length == '') {
+      $('#namecheck').show();
+      return false;
+    } else {
+      $('#namecheck').hide();
+      $('#namecheck1').show();
+      return false;
+    }
+
+  }
+  function validateSubject() {
+    let subjectValue = $('#subject').val();
+    if (subjectValue.length == '') {
+      $('#subjectcheck').show();
+      return false;
+    }
+    return true;
+  }
+  function validateMessage() {
+    let messageValue = $('#message').val();
+    if (messageValue.length == '') {
+      $('#messagecheck').show();
+      return false;
+    }
+    return true;
+  }
+  function validateEmail() {
+    let emailValue = $('#email').val();
+    let regex =
+      /^([\-\.0-9a-zA-Z]+)@([\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+
+    if (regex.test(emailValue) == true) {
+      return true;
+    } else if (emailValue.length == '') {
+      $('#emailcheck').show();
+      return false;
+    } else {
+      $('#emailcheck').hide();
+      $('#emailcheck1').show();
+      return false;
+    }
+
+  }
+
+
+  $("#submit-form").submit((e) => {
+    e.preventDefault();
+    $('#namecheck').hide();
+    $('#namecheck1').hide();
+    $('#subjectcheck').hide();
+    $('#messagecheck').hide();
+    $('#emailcheck').hide();
+    $('#emailcheck1').hide();
+    if (validateName() == false || validateEmail() == false || validateSubject() == false || validateMessage() == false) {
+      return;
+
+    }
+    $.ajax({
+      url: "https://script.google.com/macros/s/AKfycbzAEpLudP_bo6VkUAUIIHLid4Qg1utPcyVbizNVKmMrsPME5tRA92tGtz56BYQu8_8/exec",
+      data: $("#submit-form").serialize(),
+      method: "post",
+      success: function (response) {
+        alert("Form submitted successfully")
+        window.location.reload()
+        //window.location.href="https://google.com"
+      },
+      error: function (err) {
+        alert("Something Error")
+
+      }
+    });
+
+  });
+
+
+
+
+
